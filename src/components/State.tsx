0e2fdc071048c3ae5534a6f12697359d55a112d1
Grant McDade <grant.mcdade@gmail.com>
2020-12-02 11:28:48 +0100

    Remove unnecessary code

--- src/components/State.tsx
+++ src/components/State.tsx
@@ -1,6 +1,6 @@
 import { RouteComponentProps } from "@reach/router";
 import { Button, Typography } from "antd";
-import React, { useCallback } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, incrementAsync } from "../counterSlice";
 import type { RootState } from "../rootReducer";
@@ -9,23 +9,10 @@ import { Error } from "./Error";
 interface Props extends RouteComponentProps {}
 
 export const State = (props: Props) => {
-  const items: Record<string, string> = {};
   const count = useSelector((state: RootState) => state.counter.count);
   const loading = useSelector((state: RootState) => state.counter.loading);
   const error = useSelector((state: RootState) => state.counter.error);
   const dispatch = useDispatch();
-
-  const inc = useCallback(() => {
-    dispatch(increment());
-  }, [dispatch]);
-  const dec = useCallback(() => {
-    dispatch(decrement());
-  }, [dispatch]);
-  const incAsync = useCallback(() => {
-    dispatch(incrementAsync());
-  }, [dispatch]);
-
-  items["one"] = "test";
 
   return (
     <div className="content-container">
@@ -35,9 +22,26 @@ export const State = (props: Props) => {
         Test page for verifying that Flux state works
       </Typography.Paragraph>
       <Typography>{`Current Count: ${count}`}</Typography>
-      <Button onClick={inc}>Increase Count</Button>
-      <Button onClick={dec}>Decrease Count</Button>
-      <Button onClick={incAsync} loading={loading}>
+      <Button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increase Count
+      </Button>
+      <Button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrease Count
+      </Button>
+      <Button
+        onClick={() => {
+          dispatch(incrementAsync());
+        }}
+        loading={loading}
+      >
         Increase Count Async
       </Button>
     </div>
