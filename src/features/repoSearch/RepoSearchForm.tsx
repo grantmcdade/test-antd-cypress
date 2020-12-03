a01f15de34c4f2f2ad78afc73bfde2ca2b62f7a6
Grant McDade <grant.mcdade@gmail.com>
2020-12-03 13:36:46 +0100

    Modify the repo search to use antd forms

--- src/features/repoSearch/RepoSearchForm.tsx
+++ src/features/repoSearch/RepoSearchForm.tsx
@@ -1,7 +1,7 @@
-import React, { useState, ChangeEvent } from 'react'
+import React, { useEffect } from 'react'
+import { Button, Form, Input, InputNumber } from 'antd'
 
-import './pure-forms.css'
-import './pure-buttons.css'
+const useForm = Form.useForm
 
 interface Props {
   org: string
@@ -10,81 +10,73 @@ interface Props {
   setJumpToPage: (page: number) => void
 }
 
-type InputEvent = ChangeEvent<HTMLInputElement>
-type ChangeHandler = (e: InputEvent) => void
-
 export const RepoSearchForm = ({
   org,
   repo,
   setOrgAndRepo,
   setJumpToPage,
 }: Props) => {
-  const [currentOrg, setCurrentOrg] = useState(org)
-  const [currentRepo, setCurrentRepo] = useState(repo)
-  const [currentPageText, setCurrentPageText] = useState('1')
+  const [form1] = useForm()
+  const [form2] = useForm()
 
-  const onOrgChanged: ChangeHandler = (e) => {
-    setCurrentOrg(e.target.value)
+  const onForm1Finish = (values: any) => {
+    const { org, repo } = values as { org: string; repo: string }
+    setOrgAndRepo(org, repo)
   }
 
-  const onRepoChanged: ChangeHandler = (e) => {
-    setCurrentRepo(e.target.value)
-  }
+  useEffect(() => {
+    const values = {
+      org,
+      repo,
+    }
+    form1.setFieldsValue(values)
+  }, [org, repo, form1])
 
-  const onCurrentPageChanged: ChangeHandler = (e) => {
-    setCurrentPageText(e.target.value)
-  }
+  const onForm2Finish = (values: any) => {
+    const { jumpToPage } = values as { jumpToPage: number }
 
-  const onLoadRepoClicked = () => {
-    setOrgAndRepo(currentOrg, currentRepo)
-  }
-
-  const onJumpToPageClicked = () => {
-    const newPage = parseInt(currentPageText)
-
-    if (newPage >= 1) {
-      setJumpToPage(newPage)
+    if (jumpToPage >= 1) {
+      setJumpToPage(jumpToPage)
     }
   }
 
+  useEffect(() => {
+    const values = {
+      jumpToPage: 1,
+    }
+    form2.setFieldsValue(values)
+  }, [form2])
+
   return (
-    <form className="pure-form">
-      <div>
-        <label htmlFor="org" style={{ marginRight: 5 }}>
-          Org:
-        </label>
-        <input name="org" value={currentOrg} onChange={onOrgChanged} />
-        <label htmlFor="repo" style={{ marginRight: 5, marginLeft: 10 }}>
-          Repo:
-        </label>
-        <input name="repo" value={currentRepo} onChange={onRepoChanged} />
-        <button
-          type="button"
-          className="pure-button pure-button-primary"
-          style={{ marginLeft: 5 }}
-          onClick={onLoadRepoClicked}
-        >
-          Load Repo
-        </button>
-      </div>
-      <div style={{ marginTop: 5 }}>
-        <label htmlFor="jumpToPage" style={{ marginRight: 5 }}>
-          Issues Page:
-        </label>
-        <input
-          name="jumpToPage"
-          value={currentPageText}
-          onChange={onCurrentPageChanged}
-        />
-        <button
-          type="button"
-          className="pure-button pure-button-primary"
-          style={{ marginLeft: 5 }}
-          onClick={onJumpToPageClicked}
-        >
-          Jump to Page
-        </button>
-      </div>
-    </form>
+    <div>
+      <Form layout="inline" form={form1} onFinish={onForm1Finish}>
+        <Form.Item label="Org:" name="org">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Repo:" name="repo">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Load Repo
+          </Button>
+        </Form.Item>
+      </Form>
+      <Form
+        style={{ marginTop: 5 }}
+        layout="inline"
+        form={form2}
+        onFinish={onForm2Finish}
+      >
+        <Form.Item label="Issues Page:" name="jumpToPage">
+          <InputNumber min={1} />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Jump to Page
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
